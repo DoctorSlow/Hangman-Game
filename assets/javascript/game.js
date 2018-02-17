dcdf722f3974233d433463422fc40b46608cd884
Jeremy J. Lee <jeremyjlee@Jeremys-MacBook-Pro-2.local>
2018-02-17 16:25:40 -0700

    duplicate puzzles removed & game over sequence added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,4 @@
+//set our variables and intialize our arrays
 var gameCount = document.getElementById("game-Count");
 var userWins = document.getElementById("user-Wins");
 var userLosses = document.getElementById("user-Losses");
@@ -12,24 +13,37 @@ var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
     'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
 ];
 var puzzleArray = ['TOMBSTONE', 'NAVAJO', 'SAGUARO', 'TARANTULA', 'ROADRUNNER', 'CACTUS', 'JAVALINA',
-    'TORTILLA',
-    'BISBEE', 'COYOTE'
+    'BURRITO',
+    'BISBEE', 'SEDONA'
 ];
-var uniqueArray = puzzleArray.filter(function (itm, i, puzzleArray) {
-    return i == puzzleArray.indexOf(itm);
-});
+var uniqueArray = [];
 var puzzle = "";
 var correctGuesses = [];
 var guessedLetters = [];
 var answerArray = [];
 var underscoreArray = 0;
 
+//generate puzzle words
 function puzzleDisplay() {
 
     correctGuesses = [];
     guessedLetters = [];
 
     puzzle = puzzleArray[Math.floor(Math.random() * puzzleArray.length)];
+    //Generate new random words until we find one that is not in the unique array (i.e. not used before)
+    while (uniqueArray.indexOf(puzzle) != -1 && uniqueArray.length != puzzleArray.length) {
+        puzzle = puzzleArray[Math.floor(Math.random() * puzzleArray.length)];
+    }
+    //Check for end-game status and alert game over messages
+    if (uniqueArray.length == puzzleArray.length) {
+        if (wins > losses) {
+            alert("Game Over--you won! You really know your Arizona. Congratulations.");
+        } else {
+            alert("Game Over--you lost! Get outside and get to know Arizona better, already.")
+        }
+        return;
+    }
+    //intialize settings for each puzzle
     answerArray = puzzle.split("");
     underscoreArray = answerArray.length;
 
@@ -40,10 +54,10 @@ function puzzleDisplay() {
     currentPuzzle.innerHTML = correctGuesses.join(" ");
     guessesLeft.innerHTML = guessCount;
     gameCount.innerHTML = gameNum;
-    uniqueArray.push();
+    uniqueArray.push(puzzle);
 
 };
-
+//check userinput vs. puzzle letters
 function checkLetter(userInput) {
     if (alphabet.includes(userInput.toUpperCase())) {
         if (!guessedLetters.includes(userInput)) {
@@ -69,29 +83,41 @@ function checkLetter(userInput) {
         }
     }
 };
-
+//update correct & incorrect user guesses on the page
 function checkWinLoss() {
     currentPuzzle.innerHTML = correctGuesses.join(" ");
     userGuesses.innerHTML = guessedLetters.join(" ");
     guessesLeft.innerHTML = guessCount;
 
+    //update & alert win status & refresh page/set next puzzle
     if (answerArray.join(" ") === correctGuesses.join(" ")) {
         wins++;
         gameNum++;
-        alert("Answer = " + puzzle + ". Hurrah! You're a champ! Ready for another?");
         userWins.innerHTML = wins;
         userGuesses.innerHTML = "";
-        puzzleDisplay();
+        //set a timeout to allow last letters to be inputted before alerts trigger
+        window.setTimeout(function () {
+            alert("Answer = " + puzzle + ". Hurrah! Nice one.");
+            puzzleDisplay();
+        }, 300);
+
+        //update & alert loss status & refresh page/set next puzzle
     } else if (guessCount <= 0) {
         losses++;
         gameNum++;
         userLosses.innerHTML = losses;
         userGuesses.innerHTML = "";
-        alert("Oh no, you've ran out of guesses. Too bad! Answer = " + puzzle + ". Try again.");
-        puzzleDisplay();
+        //set a timeout to allow last letters to be inputted before alerts trigger
+        window.setTimeout(function () {
+            alert("Oh no, you've ran out of guesses. Too bad! Answer = " + puzzle + ".");
+            puzzleDisplay();
+        }, 300);
+
     }
+
 };
 
+//run/re-run game & convert user guesses to uppercase
 puzzleDisplay()
 document.onkeyup = function (event) {
     var userInput = String.fromCharCode(event.keyCode).toUpperCase();
